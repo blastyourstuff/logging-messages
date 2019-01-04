6a47f404c4dd92957539b1a3f4c49cc0208a6cda
Daniyar <randomna79@gmail.com>
2019-01-04 15:30:46 +0600

    Добавлены основные файлы

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# logging-messages
-Логирование изменения и удаления сообщений в Discord.
+# Логирование сообщений
+Простой способ логирования сообщений в Discord
+[<img src="https://discordapp.com/api/guilds/338202622731485185/widget.png?style=shield">](https://discord.gg/bNsgNjC)
+
+# Установка
+
+```
+git clone https://github.com/blastyourstuff/logging-messages.git
+cd logging-messages
+npm install
+```
+
+## Конфигурационный файл
+
+```
+{
+    "token": "токен_бота",
+    "logging_channel_id": "id_канала",
+    "presence": {
+        "game_name": "Discord", // Название игры
+        "type": 0, // 0 - "Играет в", 1 - "Стримит", 2 - "Слушает", 3 - "Смотрит"
+        "status": "online", // online, idle, dnd, offline
+        "game_url": "https://www.twitch.tv/blastyourstuff" // Работает только с type: 1
+    },
+    "ignore_bots": true // Если установить false, то бот будет реагировать на других ботов
+}
+```
---
+++ index.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const config = require('./config.json')
+
+bot.on("messageUpdate", async (oldmsg, newmsg) => {
+    if (oldmsg.author.bot && config.ignore_bots || oldmsg.author.id == bot.user.id) return
+
+    let log_channel = oldmsg.guild.channels.get(config.logging_channel_id)
+    if (!log_channel) return console.log('Logging channel not installed, or wrong ID is specified')
+
+    let textOldMessage = oldmsg.content.slice(0, 600), textNewMessage = newmsg.content.slice(0, 600) // Max. length in field: 1024
+    if (textOldMessage.length >= 600) textOldMessage += '...'
+    if (textNewMessage.length >= 600) textNewMessage += '...'
+
+    try {
+        let embed = new Discord.RichEmbed()
+            .setTitle('Сообщение изменено')
+            .addField('Отправитель', oldmsg.member, true)
+            .addField('Канал', oldmsg.channel.name, true)
+            .addField('Раньше', textOldMessage.length > 0 ? textOldMessage : '\u200B')
+            .addField('Сейчас', textNewMessage.length > 0 ? textNewMessage : '\u200B')
+            .setFooter(`ID Сообщения: ${oldmsg.id}`)
+            .setTimestamp()
+            .setColor(0xE19517)
+        await log_channel.send(embed)
+    } catch (err) {
+        if (['DiscordAPIError: Missing Access', 'DiscordAPIError: Missing Permissions'].some(e => e == err)) console.log(`There are no required permissions in channel ${log_channel.name}`)
+        else console.log('An unexpected error has occurred', err)
+    }
+})
+
+bot.on("messageDelete", async message => {
+    if (message.author.bot && config.ignore_bots || message.author.id == bot.user.id) return
+
+    let log_channel = message.guild.channels.get(config.logging_channel_id)
+    if (!log_channel) return console.log('Logging channel not installed, or wrong ID is specified')
+
+    let textMessage = message.content.slice(0, 600) // Max. length in field: 1024
+    if (textMessage.length >= 600) textMessage += '...'
+
+    try {
+        let embed = new Discord.RichEmbed()
+            .setTitle('Сообщение удалено')
+            .addField('Отправитель', message.member, true)
+            .addField('Канал', message.channel.name, true)
+            .addField('Содержимое', textMessage.length > 0 ? textMessage : '\u200B')
+            .setFooter(`ID Сообщения: ${message.id}`)
+            .setTimestamp()
+            .setColor(0xF04747)
+        await log_channel.send(embed)
+    } catch (err) {
+        if (['DiscordAPIError: Missing Access', 'DiscordAPIError: Missing Permissions'].some(e => e == err)) console.log(`There are no required permissions in channel ${log_channel.name}`)
+        else console.log('An unexpected error has occurred', err)
+    }
+})
+
+bot.login(config.token)
+bot.on("ready", async () => {
+    let presence = config.presence
+    await bot.user.setPresence({
+        status: presence.status,
+        game: {
+            name: presence.game_name,
+            type: presence.type,
+            url: presence.game_url ? presence.game_url : null
+        }
+    })
+    .then(console.log(`[Presence]: Status successfully installed`))
+    .catch(console.error)
+
+    await console.log(`[Start]: ${bot.user.username} connected to Discord`)
+})
